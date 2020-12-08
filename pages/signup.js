af2bcdff2b2c1e04af73304c6e681c871f0044b3
manoj07thapa <mnjinvincible@gmail.com>
2020-12-08 15:14:40 +0545

    minor fixes

--- pages/signup.js
+++ pages/signup.js
@@ -1,9 +1,7 @@
-import { useState, Fragment } from 'react';
 import fetch from 'isomorphic-unfetch';
 import { useRouter } from 'next/router';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Link from 'next/link';
@@ -88,6 +86,7 @@ export default function SignUp() {
 				router.push('/login');
 			} else {
 				actions.setErrors(data);
+				alert('signup failed');
 			}
 			actions.setSubmitting(false);
 		} catch (err) {}
